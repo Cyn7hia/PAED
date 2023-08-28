96e99720a1a69d958fb1fc4c1d876538f2f0b671
Cynthia <Luyao.Z@hotmail.com>
2023-08-28 15:40:56 +0800

    Add files via upload

---
+++ model/types_.py
@@ -0,0 +1,10 @@
+'''
+Author: Luyao Zhu, Li Wei
+Email: luyao001@e.ntu.edu.sg
+'''
+
+
+from typing import List, Callable, Union, Any, TypeVar, Tuple
+# from torch import tensor as Tensor
+
+Tensor = TypeVar('torch.tensor')
